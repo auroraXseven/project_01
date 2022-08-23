8c2f9d144e90f1b83eb778814d91f6053fd05dd7
itXie <1290453856@qq.com>
2022-08-23 10:28:53 +0800

    修改index.js和index.css

---
+++ index.js
@@ -0,0 +1,23 @@
+[33mcommit 0f7f714f782a801631bfa8536ac42d96f361bd97[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: itXie <1290453856@qq.com>
+Date:   Mon Aug 22 17:12:43 2022 +0800
+
+    修改了index.css
+
+[33mcommit 495468c2754a18734a97f6cd08ddd36826d9e2dd[m
+Author: itXie <1290453856@qq.com>
+Date:   Mon Aug 22 17:07:36 2022 +0800
+
+    修改了index.css
+
+[33mcommit fc97e504d93df3525e72cf91b73cdaf1ffb51b08[m
+Author: itXie <1290453856@qq.com>
+Date:   Mon Aug 22 16:35:20 2022 +0800
+
+    初始化了index.html
+
+[33mcommit 5cf018b05de3868d18b08a9154ddbb249ca853b4[m
+Author: itXie <1290453856@qq.com>
+Date:   Mon Aug 22 16:10:12 2022 +0800
+
+    对index.html进行了修改
